314bfc8c6cab307b48f3b6327a8655d6397c9bc1
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-11-26 13:07:36 +0100

    Handle main menu user's selection

--- app/src/main/java/com/example/sokoban33/MainActivity.java
+++ app/src/main/java/com/example/sokoban33/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.sokoban33;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -54,5 +56,18 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mainmenu, menu);
         return true;
+    }
+
+    // Handles the user's menu selection.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.levels:
+                Intent settingsActivity = new Intent(getBaseContext(), LevelsActivity.class);
+                startActivity(settingsActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
