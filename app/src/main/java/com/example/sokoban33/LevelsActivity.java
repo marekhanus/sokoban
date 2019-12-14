c3b47389f4c2881a9f2f3ba8e7eea975da62f3f4
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-14 15:05:34 +0100

    Render levels list in LevelsActivy

--- app/src/main/java/com/example/sokoban33/LevelsActivity.java
+++ app/src/main/java/com/example/sokoban33/LevelsActivity.java
@@ -2,6 +2,9 @@ package com.example.sokoban33;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class LevelsActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class LevelsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_levels, arrayList);
+
+        ListView listView = (ListView)findViewById(R.id.levels_list);
+        listView.setAdapter(adapter);
     }
 }
