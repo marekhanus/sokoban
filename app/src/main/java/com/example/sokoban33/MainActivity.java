257a735c39d052108420d125483bb5b117cb3e8a
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-14 17:54:39 +0100

    Take picture when create level menu item is selected

--- app/src/main/java/com/example/sokoban33/MainActivity.java
+++ app/src/main/java/com/example/sokoban33/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sokoban33;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -66,8 +67,19 @@ public class MainActivity extends AppCompatActivity {
                 Intent levelsActivity = new Intent(getBaseContext(), LevelsActivity.class);
                 startActivity(levelsActivity);
                 return true;
+            case R.id.customLevel:
+                dispatchTakePictureIntent();
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 }
