de2269e3967f7ca130b7a9f5172a725245bcbb8f
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-14 16:39:38 +0100

    Add constants for bitmap to SokoView

--- app/src/main/java/com/example/sokoban33/SokoView.java
+++ app/src/main/java/com/example/sokoban33/SokoView.java
@@ -16,6 +16,12 @@ import java.io.InputStream;
  */
 
 public class SokoView extends View{
+    public static final int EMPTY = 0;
+    public static final int WALL = 1;
+    public static final int BOX = 2;
+    public static final int GOAL = 3;
+    public static final int HERO = 4;
+    public static final int BOXOK = 5;
 
     Bitmap[] bmp;
 
