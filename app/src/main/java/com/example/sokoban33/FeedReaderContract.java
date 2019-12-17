2f08a1d8f553c65481b6b8f41c78d4602bbc4925
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-17 15:17:19 +0100

    Create a database using an SQL helper

--- app/src/main/java/com/example/sokoban33/FeedReaderContract.java
+++ app/src/main/java/com/example/sokoban33/FeedReaderContract.java
@@ -7,6 +7,15 @@ public final class FeedReaderContract {
     // make the constructor private.
     private FeedReaderContract() {}
 
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+                    FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
     /* Inner class that defines the table contents */
     public static class FeedEntry implements BaseColumns {
         public static final String TABLE_NAME = "entry";
