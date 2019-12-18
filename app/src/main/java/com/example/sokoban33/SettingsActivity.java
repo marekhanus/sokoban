39f445d40a5eb74b669178f86dc4d046cbd7e5b2
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-18 15:53:19 +0100

    Save sound setting on change to shared preferences

--- app/src/main/java/com/example/sokoban33/SettingsActivity.java
+++ app/src/main/java/com/example/sokoban33/SettingsActivity.java
@@ -1,13 +1,29 @@
 package com.example.sokoban33;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Switch soundSwitch = findViewById(R.id.switch1);
+        soundSwitch.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                SharedPreferences preferences = getApplicationContext().getSharedPreferences(
+                        "com.example.sokoban33",
+                        android.content.Context.MODE_PRIVATE
+                );
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("sound", b);
+                editor.commit();
+            }
+        });
     }
 }
