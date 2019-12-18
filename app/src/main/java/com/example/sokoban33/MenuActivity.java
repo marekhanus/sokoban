682c8469152b3bc1059682c5e943e274888ed490
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-18 16:46:54 +0100

    Handle button on click actions in MenuActivity

--- app/src/main/java/com/example/sokoban33/MenuActivity.java
+++ app/src/main/java/com/example/sokoban33/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.sokoban33;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void newame(View view) {
+        Intent mainActivity = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(mainActivity);
+    }
+
+    public void levels(View view) {
+        Intent levelsActivity = new Intent(getBaseContext(), LevelsActivity.class);
+        startActivity(levelsActivity);
+    }
+
+    public void settings(View view) {
+
+        Intent settingsActivity = new Intent(getBaseContext(), SettingsActivity.class);
+        startActivity(settingsActivity);
     }
 }
