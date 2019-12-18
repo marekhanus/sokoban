9c5c050136f4098d5c9b72b7aa8008cb6c185fdf
Marek Hanuš <marek.hanus.st@vsb.cz>
2019-12-18 17:21:28 +0100

    Skip selection from DB causing 0 returned rows

--- app/src/main/java/com/example/sokoban33/SokoView.java
+++ app/src/main/java/com/example/sokoban33/SokoView.java
@@ -81,8 +81,8 @@ public class SokoView extends View{
         };
 
         // Filter results WHERE "title" = 'My Title'
-        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_LEVEL + " = ?";
-        String[] selectionArgs = { "My Title" };
+//        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_LEVEL + " = ?";
+//        String[] selectionArgs = { "My Title" };
 
         // How you want the results sorted in the resulting Cursor
         String sortOrder =
@@ -91,8 +91,8 @@ public class SokoView extends View{
         Cursor cursor = db.query(
                 FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
                 projection,             // The array of columns to return (pass null to get all)
-                selection,              // The columns for the WHERE clause
-                selectionArgs,          // The values for the WHERE clause
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
                 null,                   // don't group the rows
                 null,                   // don't filter by row groups
                 sortOrder               // The sort order
